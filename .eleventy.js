e114c9a4c5ca0d83a90504ed3808d24c29d403de
Lucas Fischer <lucas.fischer@me.com>
2025-11-05 14:38:04 +0800

    add sitemap generation via @quasibit/eleventy-plugin-sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,27 @@
 import { HtmlBasePlugin } from "@11ty/eleventy";
 import pugPlugin from "@11ty/eleventy-plugin-pug";
-import pluginRss from "@11ty/eleventy-plugin-rss"
+import pluginRss from "@11ty/eleventy-plugin-rss";
+import sitemap from "@quasibit/eleventy-plugin-sitemap";
 
 export default function(config) {
   config.addPassthroughCopy("./assets")
   config.addPlugin(HtmlBasePlugin);
   config.addPlugin(pugPlugin);
   config.addPlugin(pluginRss)
+  config.addPlugin(sitemap, {
+      sitemap: {
+        hostname: "https://kinopio.club",
+      },
+  });
+  
+  config.addCollection("sitemap", (collectionApi) => {
+    return collectionApi
+      .getAll()
+      .map((item, index, all) => {
+        item.url = "/blog" + item.url;
+        return item;
+      });
+  });
   config.setQuietMode(true)
 }
 
